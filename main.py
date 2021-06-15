527fd64d8261aa289ba1872c11af500b1a3a8e14
Yulin Zhu <Y.Zhu@wzl.rwth-aachen.de>
2021-06-15 08:27:49 +0200

    add a rough view of GUI, delete unnecessary venv/ .

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import GUI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication(sys.argv)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    mainW = QMainWindow()
+    ui = GUI.Ui_MainWindow()
+    ui.setupUi(mainW)
+
+    mainW.show()
+    sys.exit(app.exec_())
+
