0d1293817838e7b4d89f1e6c975f205ccbfea50d
Yulin Zhu <zieft@hotmail.com>
2021-11-08 14:10:59 +0100

    initialization of k8mars_control.py

---
+++ core/k8mars_control.py
@@ -0,0 +1,20 @@
+import subprocess
+
+class K8marsSession(object):
+    def __init__(self):
+        pass
+
+class k8marsController(object):
+    pass
+
+class YamlFileSession(object):
+    pass
+
+class YamlFileController(object):
+    pass
+
+class DockerFileSession(object):
+    pass
+
+class DockerFileController(object):
+    pass
